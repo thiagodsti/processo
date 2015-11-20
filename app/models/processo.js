3782ea8b205efd6659073cb1aca7386cff8c4813
Thiago Diniz da Silveira <thiago.silveira@supero.com.br>
2015-11-20 10:37:09 -0200

    First Commit

---
+++ app/models/processo.js
@@ -0,0 +1,9 @@
+var mongoose     = require('mongoose');
+var Schema       = mongoose.Schema;
+
+var ProcessoSchema   = new Schema({
+    processo: String,
+	fiscal: String
+});
+
+module.exports = mongoose.model('Processo', ProcessoSchema);
