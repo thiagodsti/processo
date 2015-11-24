1e5b8e39e6e4ab63b8f5872ca84b432b21ae3443
Thiago Diniz <thiagods.ti@gmail.com>
2015-11-24 00:14:36 -0200

    Tabela processos

--- app/js/controllers/processoController.js
+++ app/js/controllers/processoController.js
@@ -1,12 +1,11 @@
 	angular.module("processo").controller("processoController", function($scope, $filter, processoService){
 
-
-    console.log('cheguei aqui');
-
     processoService.getProcesso().success(function (data, status) {
       console.log(data);
     }).error(function (data, status) {
       console.log("Não foi possível carregar os dados!");
     });
+
+		scope.processos = [];
 
   });
--- server/models/processo/entity.js
+++ server/models/processo/entity.js
@@ -2,7 +2,7 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var ProcessoSchema   = new Schema({
-    processo: String,
+  processo: String,
 	fiscal: String
 });
 
