53c95802ac7ba2893684db7ebd8794e6ff7c86e1
Thiago <thiago@linux-xlvq.suse>
2015-11-28 11:56:01 -0200

    Salvament funcionando, falta arrumar agora o CSS e continuar colocando os campos do processo

--- app/js/controllers/processoController.js
+++ app/js/controllers/processoController.js
@@ -1,11 +1,31 @@
 	angular.module("processo").controller("processoController", function($scope, $filter, processoService){
 
+		$scope.processos = [];
+		$scope.processo = {};
+
     processoService.getProcesso().success(function (data, status) {
       console.log(data);
     }).error(function (data, status) {
       console.log("Não foi possível carregar os dados!");
     });
 
-		$scope.processos = [];
+		$scope.adicionarProcesso = function (processo){
+			console.log(processo);
+			processoService.saveProcesso(processo).then(function() {
+				console.log('salvo mesmo com sucesso');
+			});
+		};
+
+		$scope.removerProcesso = function (processo) {
+			console.log(processo);
+		};
+
+		$scope.isProcessoSelecionado = function (processo) {
+			//return processos.some(function (processo) {
+			//	return processo.selecionado;
+			//});
+		};
+
+
 
   });
--- app/js/services/processoService.js
+++ app/js/services/processoService.js
@@ -1,10 +1,17 @@
-angular.module("processo").factory("processoService", function($http, config) {
+angular.module("processo").factory("processoService", function($http, $q, config) {
 		var _getProcesso = function () {
 			return $http.get(config.baseUrl + "/processos");
 		};
 
-		var _saveProcesso = function (contato) {
-		//	return $http.post(config.baseUrl + "/CadastroHoras/rest/usuarios", contato);
+		var _saveProcesso = function (processo) {
+			var deferred = $q.defer();
+			$http.post(config.baseUrl + "/processos", processo).success(function() {
+				console.log('sucesso');
+			}).error(function (msg, code) {
+				deferred.reject(msg);
+				console.log('erro ' + code);
+			});
+			return deferred.promise;
 		};
 
 		return {
